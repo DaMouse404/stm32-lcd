f0c85124f85c7651630a7ef48862193342d0e67d
Christopher Sidebottom <christopher.sidebottom@bbc.co.uk>
2015-03-14 15:48:37 +0000

    Clean up LED test

--- test/led_test.c
+++ test/led_test.c
@@ -9,7 +9,6 @@
 void rcc_periph_clock_enable(enum rcc_periph_clken i)
 {
 	check_expected(i);
-	mock();
 }
 
 void gpio_mode_setup(uint32_t port, uint8_t mode, uint8_t pullup, uint16_t pin)
@@ -18,35 +17,30 @@ void gpio_mode_setup(uint32_t port, uint8_t mode, uint8_t pullup, uint16_t pin)
 	check_expected(mode);
 	check_expected(pullup);
 	check_expected(pin);
-	mock();
 }
 
 void gpio_toggle(uint32_t port, uint16_t pin)
 {
 	check_expected(port);
 	check_expected(pin);
-	mock();
 }
 
-static void test_led_setup(void)
+static void test_led_setup()
 {
 	expect_value(rcc_periph_clock_enable, i, RCC_GPIOE);
-	will_return(rcc_periph_clock_enable, 0);
 
 	expect_value(gpio_mode_setup, port, GPIOE);
 	expect_value(gpio_mode_setup, mode, GPIO_MODE_OUTPUT);
 	expect_value(gpio_mode_setup, pullup, GPIO_PUPD_NONE);
 	expect_value(gpio_mode_setup, pin, GPIO12);
-	will_return(gpio_mode_setup, 0);
 
 	led_setup();
 }
 
-static void test_led_toggle(void)
+static void test_led_toggle()
 {
 	 expect_value(gpio_toggle, port, GPIOE);
 	 expect_value(gpio_toggle, pin, GPIO12);
-	 will_return(gpio_toggle, 0);
 
 	 led_toggle();
 }
